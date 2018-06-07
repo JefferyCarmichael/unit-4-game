c4d4469ef45c9bde82541bf64dc0e58d4fcd78b3
Jeff2C <jeff.c.carmichael@gmail.com>
2018-06-06 22:19:21 -0400

    Homework 4

---
+++ assets/javascript/game.js
@@ -0,0 +1,83 @@
+
+
+// Initial setup for game board.
+var target = Math.floor(Math.random() * 101 + 20);
+var jewel1 = Math.floor(Math.random() * 12 + 1);
+var jewel2 = Math.floor(Math.random() * 11 + 1);
+var jewel3 = Math.floor(Math.random() * 12 + 1);
+var jewel4 = Math.floor(Math.random() * 11 + 1);
+var total = 0;
+var wins = 0;
+var losses = 0;
+$("#random-number").html(target);
+$("#current-score-counter").html(total);
+
+
+
+// Click functions for the 4 jewels
+
+
+$("#jewel1").on("click", function () {
+    total = total + jewel1;
+
+    $("#current-score-counter").html(total);
+    play();
+});
+
+
+$("#jewel2").on("click", function () {
+    total = total + jewel2;
+
+    $("#current-score-counter").html(total);
+    play();
+});
+
+$("#jewel3").on("click", function () {
+    total = total + jewel3;
+
+    $("#current-score-counter").html(total);
+    play();
+});
+
+$("#jewel4").on("click", function () {
+    total = total + jewel4;
+
+    $("#current-score-counter").html(total);
+    play();
+});
+
+//Checks score to determine if player won or loss.
+function play() {
+
+    if (total == target) {
+        wins++;
+        $("#current-score-counter").html(total);
+        alert(total + ": you win");
+        console.log("wins:" + wins);
+        $("#win-counter").html(wins);
+        shuffle();
+    }
+
+    else if (total > target) {
+        losses++;
+        $("#current-score-counter").html(total);
+        alert(total + ": you lose");
+
+        $("#loss-counter").html(losses);
+        shuffle();
+    }
+
+}
+// Clears board after win or loss.
+function shuffle() {
+    target = Math.floor(Math.random() * 101 + 20);
+    jewel1 = Math.floor(Math.random() * 12 + 1);
+    jewel2 = Math.floor(Math.random() * 11 + 1);
+    jewel3 = Math.floor(Math.random() * 12 + 1);
+    jewel4 = Math.floor(Math.random() * 11 + 1);
+    total = 0;
+    $("#random-number").html(target);
+    $("#current-score-counter").html(total);
+
+}
+
